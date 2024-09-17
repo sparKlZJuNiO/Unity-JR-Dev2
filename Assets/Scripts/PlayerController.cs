7c50a43a55e7afee2abf8f720a2491882641b3c6
sparKlZJuNiO <jrrossjob@gmail.com>
2024-09-17 10:44:35 +0100

    Firstt pushy

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Create a reference to the Rigidbody2D so we can manipulate it
+    Rigidbody2D playerObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Find the Rigidbody2D component that is attached to the same object as this script
+        playerObject = GetComponent<Rigidbody2D>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Create a 'float' that will be equal to the players horizontal input
+        float movementValueX = Input.GetAxis("Horizontal");
+
+
+        // Change the X velocity of the Rigidbody2D to be equal to the movement value
+        playerObject.velocity = new Vector2(movementValueX, playerObject.velocity.y);
+    }
+}
